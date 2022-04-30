08d34fc472c34da69b2bb9e6699fc78d4b877a1c
Craig Thacker <craigthackerx@gmail.com>
2022-05-01 00:52:55 +0100

    Update module

--- input.tf
+++ input.tf
@@ -1,14 +1,31 @@
-variable "rg_name" {
-  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
-  type        = string
-  validation {
-    condition     = length(var.rg_name) > 1 && length(var.rg_name) <= 24
-    error_message = "Resource group name is not valid."
-  }
+variable "allocated_outbound_ports" {
+  description = "The number of ports allocated for the outbound rule"
+  type        = number
 }
 
-variable "location" {
-  description = "The location for this resource to be put in"
+variable "allocation_method" {
+  description = "Defines how an IP address is assigned. Options are Static or Dynamic."
+  type        = string
+  default     = "Dynamic"
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the PIP to be created to"
+  type        = list(any)
+}
+
+variable "enable_outbound_rule" {
+  description = "Whether an outbound rule should be made"
+  type        = bool
+}
+
+variable "lb_bpool_name" {
+  description = "The name for the backend pool for the Load Balancer"
+  type        = string
+}
+
+variable "lb_ip_configuration_name" {
+  description = "The name of the frontend IP Configuration name"
   type        = string
 }
 
@@ -23,23 +40,8 @@ variable "lb_sku_name" {
   default     = "Standard"
 }
 
-variable "lb_ip_configuration_name" {
-  description = "The name of the frontend IP Configuration name"
-  type        = string
-}
-
-variable "lb_bpool_name" {
-  description = "The name for the backend pool for the Load Balancer"
-  type        = string
-}
-
-variable "enable_outbound_rule" {
-  description = "Whether an outbound rule should be made"
-  type        = bool
-}
-
-variable "outbound_rule_name" {
-  description = "The name of the outbound rule"
+variable "location" {
+  description = "The location for this resource to be put in"
   type        = string
 }
 
@@ -48,20 +50,8 @@ variable "outbound_protocol" {
   description = "The protocol for the outbound rule"
 }
 
-variable "allocated_outbound_ports" {
-  description = "The number of ports allocated for the outbound rule"
-  type        = number
-}
-
-variable "pip_sku" {
-  default     = null
-  description = "If you wish to assign a public IP directly, set this to Standard"
-  type        = string
-}
-
-variable "pip_name" {
-  default     = null
-  description = "If you are using a Public IP, set the name in this variable"
+variable "outbound_rule_name" {
+  description = "The name of the outbound rule"
   type        = string
 }
 
@@ -70,9 +60,25 @@ variable "pip_custom_dns_label" {
   description = "If you are using a public IP and wish to assign a custom DNS label, set here, otherwise, the VM host name will be used"
 }
 
-variable "availability_zone" {
-  description = "The availability zone for the PIP to be created to"
-  type        = list(any)
+variable "pip_name" {
+  default     = null
+  description = "If you are using a Public IP, set the name in this variable"
+  type        = string
+}
+
+variable "pip_sku" {
+  default     = null
+  description = "If you wish to assign a public IP directly, set this to Standard"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
+  type        = string
+  validation {
+    condition     = length(var.rg_name) > 1 && length(var.rg_name) <= 24
+    error_message = "Resource group name is not valid."
+  }
 }
 
 variable "tags" {
@@ -82,10 +88,4 @@ variable "tags" {
   default = {
     source = "terraform"
   }
-}
-
-variable "allocation_method" {
-  description = "Defines how an IP address is assigned. Options are Static or Dynamic."
-  type        = string
-  default     = "Dynamic"
 }
--- output.tf
+++ output.tf
@@ -1,18 +1,3 @@
-output "lb_id" {
-  value       = azurerm_lb.pub_lb.id
-  description = "The ID of the load balancer"
-}
-
-output "lb_name" {
-  value       = azurerm_lb.pub_lb.name
-  description = "The Name of the load balancer"
-}
-
-output "lb_ip_configuration" {
-  value       = azurerm_lb.pub_lb.frontend_ip_configuration
-  description = "The frontend ip configuration object"
-}
-
 output "bpool_id" {
   value       = azurerm_lb_backend_address_pool.public_lb_bpool.id
   description = "The id of the backend pool"
@@ -24,19 +9,24 @@ output "bpool_name" {
   description = "The name of the backend pool"
 }
 
-output "pip_id" {
-  value       = azurerm_public_ip.pip.*.id
-  description = "The id of the public ip"
+output "lb_id" {
+  value       = azurerm_lb.pub_lb.id
+  description = "The ID of the load balancer"
 }
 
-output "pip_name" {
-  value       = azurerm_public_ip.pip.*.name
-  description = "The name of the public ip"
+output "lb_ip_configuration" {
+  value       = azurerm_lb.pub_lb.frontend_ip_configuration
+  description = "The frontend ip configuration object"
 }
 
-output "pip_ip_address" {
-  value       = azurerm_public_ip.pip.*.ip_address
-  description = "The address of the public ip"
+output "lb_name" {
+  value       = azurerm_lb.pub_lb.name
+  description = "The Name of the load balancer"
+}
+
+output "outbound_allocated_outbound_ports" {
+  value       = azurerm_lb_outbound_rule.outbound_rule.*.allocated_outbound_ports
+  description = "The allocated ports of the outbound rule if created"
 }
 
 output "outbound_rule_id" {
@@ -54,7 +44,17 @@ output "outbound_rule_protocol" {
   description = "The protocl of the outbound rule if created"
 }
 
-output "outbound_allocated_outbound_ports" {
-  value       = azurerm_lb_outbound_rule.outbound_rule.*.allocated_outbound_ports
-  description = "The allocated ports of the outbound rule if created"
+output "pip_id" {
+  value       = azurerm_public_ip.pip.*.id
+  description = "The id of the public ip"
+}
+
+output "pip_ip_address" {
+  value       = azurerm_public_ip.pip.*.ip_address
+  description = "The address of the public ip"
+}
+
+output "pip_name" {
+  value       = azurerm_public_ip.pip.*.name
+  description = "The name of the public ip"
 }
